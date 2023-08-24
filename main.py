1de16c1cc88306f36e8b877e319a104fbf382abc
Maksim Surov <maksim.surov13@yandex.ru>
2023-08-24 12:50:28 +0300

    added new descriptions for functions

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from random import randint
 from graphic_arts.start_game_banner import run_screensaver
 
+
 def attack(char_name: str,
            char_class: str) -> str:
+    """Описание функции атаки."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику'
                 f' равный {5 + randint(3, 5)}')
@@ -16,6 +18,7 @@ def attack(char_name: str,
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Описание функции защиты."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -26,6 +29,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Описание функции спецаильно умения."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение'
                 f' «Выносливость {80 + 25}»')
@@ -37,6 +41,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Описание функции начала тренировки."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -61,6 +66,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Функция выбора класса персонажа."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -93,6 +99,3 @@ if __name__ == '__main__':
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-
