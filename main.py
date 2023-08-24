f8524421a8150cb29d422610ce16df028610d7b8
Maksim Surov <maksim.surov13@yandex.ru>
2023-08-24 11:27:59 +0300

    New graphics

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from random import randint
-
+from graphic_arts.start_game_banner import run_screensaver
 
 def attack(char_name: str,
            char_class: str) -> str:
@@ -82,7 +82,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -94,4 +95,4 @@ def main():
     print(start_training(char_name, char_class))
 
 
-main()
+
